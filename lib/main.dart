67773a7478ddd5396b95a54424059b13a73e3779
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-10-14 16:32:23 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/sign_in_page.dart';
+import 'theme.dart';
+
+// Replace with your Supabase values
+const String SUPABASE_URL = 'https://hwnrfdorpsazrujmoxhl.supabase.co';
+const String SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3bnJmZG9ycHNhenJ1am1veGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDg1NDQsImV4cCI6MjA3NTEyNDU0NH0.iynHcMIAVTPxaoYL94OldQnLh7DD0SRJkaTXg7ckGc8';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(url: SUPABASE_URL, anonKey: SUPABASE_ANON_KEY);
+  runApp(const QuizMasterApp());
+}
+
+class QuizMasterApp extends StatelessWidget {
+  const QuizMasterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz Master',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const SignInPage(),
+    );
+  }
+}
+/*import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'pages/sign_in_page.dart';
 
 // Supabase constants
 const String SUPABASE_URL = 'https://hwnrfdorpsazrujmoxhl.supabase.co';
@@ -40,35 +68,4 @@ class QuizMasterApp extends StatelessWidget {
       home: const SignInPage(),
     );
   }
-}
-/*import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'pages/sign_in_page.dart';
-
-//the link is from the api
-const String SUPABASE_URL = 'https://hwnrfdorpsazrujmoxhl.supabase.co';
-const String SUPABASE_ANON_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3bnJmZG9ycHNhenJ1am1veGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDg1NDQsImV4cCI6MjA3NTEyNDU0NH0.iynHcMIAVTPxaoYL94OldQnLh7DD0SRJkaTXg7ckGc8';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(url: SUPABASE_URL, anonKey: SUPABASE_ANON_KEY);
-  runApp(const QuizMasterApp());
-}
-
-class QuizMasterApp extends StatelessWidget {
-  const QuizMasterApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Quiz Master',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.green[50],
-      ),
-      home: const SignInPage(),
-    );
-  }
-}
-*/
+}*/
