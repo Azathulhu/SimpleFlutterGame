f3f9a40320cf8328db22f9c36a0dccce0f0948e9
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-10-15 09:03:56 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/sign_in_page.dart';
 import 'theme.dart';
-
+//
 // Replace with your Supabase values (yours already present)
 const String SUPABASE_URL = 'https://hwnrfdorpsazrujmoxhl.supabase.co';
 const String SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3bnJmZG9ycHNhenJ1am1veGhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NDg1NDQsImV4cCI6MjA3NTEyNDU0NH0.iynHcMIAVTPxaoYL94OldQnLh7DD0SRJkaTXg7ckGc8';
