c3c98ae9b6bac8f2f125422b9502d2158a9eb680
Azathulhu <65434073+Azathulhu@users.noreply.github.com>
2025-10-15 09:09:42 +0800

    Update sign_in_page.dart

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -4,6 +4,7 @@ import '../services/auth_service.dart';
 import 'sign_up_page.dart';
 import 'home_page.dart';
 import '../theme.dart';
+import '../animated_background.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
